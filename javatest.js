496a8c81a2eb808e3a5e4ccce5d12960f84b1b27
GabeFernandez <34356781+GabeFernandez@users.noreply.github.com>
2017-12-07 17:25:06 -0800

    Add files via upload

--- javatest.js
+++ javatest.js
@@ -1,5 +1,5 @@
 function init() {
- Tabletop.init( { key: ‘1qCEL0QwQP8tYbf5LtYuwuMTb6KQWnlMWO2zjWZEGMrQ',
+ Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1qCEL0QwQP8tYbf5LtYuwuMTb6KQWnlMWO2zjWZEGMrQ/edit#gid=0',
  callback: function(data, tabletop) { 
  console.log(data)
  },
